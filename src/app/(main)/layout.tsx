3087cad772b0cc3a6bc0cb464b986a588a86988f
yumitaito <ninjamankk2@yahoo.co.jp>
2025-03-08 21:58:13 +0900

    メインページの実装

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,7 +4,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen ">
       <SideMenu />
-      <main className=" bg-red-300 flex-1 overflow-auto">{children}</main>
+      <main className=" bg-slate-50 flex-1 overflow-auto">{children}</main>
     </div>
   );
 };
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import Link from "next/link";
+import { MdAddTask } from "react-icons/md";
+
+export default function MainPage() {
   return (
-    <div>
-      <h1>Hello Next.js!</h1>
+    <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
+      <header className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold">All Tasks</h1>
+        <Link
+          href="/new"
+          className="flex items-center gap-1 font-semibold border px-4 py-2 rounded-full shadow-sm text-white bg-gray-800 hover:bg-gray-700"
+        >
+          <MdAddTask className="size-5" />
+          <div>Add Task</div>
+        </Link>
+      </header>
+      <div className="mt-8 flex flex-wrap gap-4">Task Card</div>
     </div>
   );
 }
