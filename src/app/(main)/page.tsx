fcf8f0e1098c489a7d67f40bf947b7c3853a9846
yumitaito <ninjamankk2@yahoo.co.jp>
2025-03-09 08:38:04 +0900

    メインページからタスク一覧の取得

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,21 @@
 import Link from "next/link";
 import { MdAddTask } from "react-icons/md";
 import TaskCard from "../../components/TaskCard/TaskCard";
+import { TaskDocument } from "@/models/task";
 
-export default function MainPage() {
+const getAllTasks = async (): Promise<TaskDocument[]> => {
+  const response = await fetch(`${process.env.API_URL}/tasks`, {
+    cache: "no-store",
+  });
+  if (response.status !== 200) {
+    throw new Error("");
+  }
+  const data = await response.json();
+  return data.tasks as TaskDocument[];
+};
+
+export default async function MainPage() {
+  const allTasks = await getAllTasks();
   return (
     <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
       <header className="flex justify-between items-center">
@@ -16,7 +29,9 @@ export default function MainPage() {
         </Link>
       </header>
       <div className="mt-8 flex flex-wrap gap-4">
-        <TaskCard />
+        {allTasks.map((task) => (
+          <TaskCard key={task._id} task={task} />
+        ))}
       </div>
     </div>
   );
--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,27 +1,35 @@
 import TaskEditButton from "./TaskEditButton/TaskEditButton";
 import TaskDeleteButton from "./TaskDeleteButton/TaskDeleteButton";
-const TaskCard = () => {
+import { TaskDocument } from "@/models/task";
+
+interface TaskCardProps {
+  task: TaskDocument;
+}
+
+const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
   return (
     <div
       className="w-64 h-52 p-4 bg-white rounded-md shadow-md 
   flex flex-col justify-between"
     >
       <header>
-        <h1 className="text-lg font-semibold">タイトル</h1>
-        <div className="mt-1 text-sm line-clamp-3">タスクの説明</div>
+        <h1 className="text-lg font-semibold">{task.title}</h1>
+        <div className="mt-1 text-sm line-clamp-3">{task.description}</div>
       </header>
       <div>
-        <div className="text-sm">2024-12-31</div>
+        <div className="text-sm">{task.dueDate}</div>
         <div className="flex justify-between items-center">
           <div
             className={`mt-1 text-sm px-2 py-1 w-24 text-center text-white 
-        rounded-full shadow-sm ${true ? "bg-green-500" : "bg-red-500"}`}
+        rounded-full shadow-sm ${
+          task.isCompleted ? "bg-green-500" : "bg-red-500"
+        }`}
           >
-            {true ? "Completed" : "Incomplete"}
+            {task.isCompleted ? "Completed" : "Incomplete"}
           </div>
           <div className="flex gap-4">
-            <TaskEditButton id="1" />
-            <TaskDeleteButton id="1" />
+            <TaskEditButton id={task._id} />
+            <TaskDeleteButton id={task._id} />
           </div>
         </div>
       </div>
