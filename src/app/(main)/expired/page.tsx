ea3ebd9e686d510b3815ecf3b8aac928f1e0a2d9
yumitaito <ninjamankk2@yahoo.co.jp>
2025-03-09 10:21:58 +0900

    期限切れタスクページの実装

--- src/app/(main)/expired/page.tsx
+++ src/app/(main)/expired/page.tsx
@@ -1,13 +1,27 @@
 import TaskCard from "../../../components/TaskCard/TaskCard";
+import { TaskDocument } from "@/models/task";
+const getExpiredTasks = async (): Promise<TaskDocument[]> => {
+  const response = await fetch(`${process.env.API_URL}/tasks/expired`, {
+    cache: "no-store",
+  });
+  if (response.status !== 200) {
+    throw new Error("");
+  }
+  const data = await response.json();
+  return data.tasks as TaskDocument[];
+};
 
-const ExpiredTasksPage = () => {
+const ExpiredTasksPage = async () => {
+  const expiredTasks = await getExpiredTasks();
   return (
     <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
       <header className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">Expired Tasks</h1>
       </header>
       <div className="mt-8 flex flex-wrap gap-4">
-        <TaskCard />
+        {expiredTasks.map((task) => (
+          <TaskCard key={task._id} task={task} />
+        ))}
       </div>
     </div>
   );
